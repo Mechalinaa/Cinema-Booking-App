8efad80158a389f5812306427574ec92f2cf7e18
Mechalinaa <lenska.michalina@gmail.com>
2026-01-05 18:03:55 +0100

    etap 1

--- src/main/java/com/example/cinema_booking/security/web/HomeController.java
+++
@@ -1,4 +0,0 @@
-package com.example.cinema_booking.security.web;
-
-public class HomeController {
-}
---
+++ src/main/java/com/example/cinema_booking/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.cinema_booking.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
